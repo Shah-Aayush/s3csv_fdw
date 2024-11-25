63df626202f105e1325fd4ce7c5cb2191c8e21e5
Ajaysinh Rathod <Ajaysinh.Rathod1@ibm.com>
2024-11-26 00:02:37 +0530

    bigint bug fixed

--- s3fdw/s3fdw.py
+++ s3fdw/s3fdw.py
@@ -232,6 +232,10 @@ class S3Fdw(ForeignDataWrapper):
                             if max_len > 0 and len(value) > max_len:
                                 value = value[:max_len]
 
+                    # If the column is supposed to be BIGINT, convert empty strings to None
+                    if col_def.type_name == 'bigint' and value == '':
+                        value = None  # Set empty string to None (NULL in SQL)
+
                 processed_row.append(value)
             except Exception as e:
                 # Add more detailed logging here to trace the exact issue
